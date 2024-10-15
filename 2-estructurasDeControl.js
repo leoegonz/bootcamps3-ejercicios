a55619032fc4992a3cfb6786854280dafe566dd4
leo <sofialespinola@gmail.com>
2024-10-15 00:35:52 -0300

    Modificada la ultima funcion para que evalue los numeros

--- 2-estructurasDeControl.js
+++ 2-estructurasDeControl.js
@@ -114,18 +114,23 @@ function esVocal(letra) {
 }
 
 // Ejercicio 10: Condicionales complejos con operadores lógicos
-// Instrucción: Escribe una función que reciba tres números y determine si todos son positivos, al menos uno es negativo, o todos son negativos. Usa operadores lógicos (`&&`, `||`).
+// Instrucción: Escribe una función que reciba tres números y determine 
+//si todos son positivos, al menos uno es negativo, o todos son negativos. 
+//Usa operadores lógicos (`&&`, `||`).
 function evaluarNumeros(a, b, c) {
     if (a > 0 && b > 0 && c > 0) {
         console.log("Todos son positivos");
-    } else if (a < 0 || b < 0 || c < 0) {
-        console.log("Al menos uno es negativo");
     } else if (a < 0 && b < 0 && c < 0) {
         console.log("Todos son negativos");
+    } else if (a < 0 || b < 0 || c < 0) {
+        console.log("Al menos uno es negativo");
     } else {
         console.log("Hay una mezcla de positivos y negativos");
     }
 }
+
+//evaluarNumeros(1, 2, 3);
+
 
 // Exportar todas las funciones
 export {
